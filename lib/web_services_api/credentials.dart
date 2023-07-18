ea9d5ecf9f1c6ff3ab31fe8a5777d21168ecad03
José Miguel Márquez Herreros <josemiguelmarquezherreros@gmail.com>
2023-07-18 19:35:42 +0200

    Update credentials.dart

--- lib/web_services_api/credentials.dart
+++ lib/web_services_api/credentials.dart
@@ -3,7 +3,8 @@ const String getTokenUrl = "http://projects.ugr.es:8089/api/users/current";
 const String postDataPointUrl = "http://projects.ugr.es:8089/api/deployments/null/data-points";
 const String batchPostDataPointUrl = "http://projects.ugr.es:8089/api/deployments/null/data-points/batch";
 
-const String clientID = "carp";
-const String clientSecret = "carp";
-const String username = "googlePlay@test.dk";
-const String password = "googlePlay";
+// To protect the server the credentials have been deleted
+const String clientID = "";
+const String clientSecret = "";
+const String username = "";
+const String password = "";
